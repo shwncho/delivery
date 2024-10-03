af684740be2132bbcebdac1fbd0875702fbbc76b
shwncho <shwncho4@gmail.com>
2024-10-03 19:54:26 +0900

    feat: create delivery api project

---
+++ delivery-api/src/main/kotlin/com/delivery/deliveryapi/interfaces/HealthController.kt
@@ -0,0 +1,11 @@
+package com.delivery.deliveryapi.interfaces
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun health() = "health"
+}
